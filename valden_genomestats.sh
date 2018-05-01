41751b9671e2e558824ad75d1b0ebf305d280721
ZackBoyd123 <zacharyboyd123@gmail.com>
2018-05-01 16:16:31 +0100

    Update valden_genomestats.sh

--- valden_genomestats.sh
+++ valden_genomestats.sh
@@ -1,5 +1,5 @@
-GenomeStats.py $OPT_R
-mdust $OPT_R > ${PWD##*/}".dust.fasta"
+GenomeStats.py $1
+mdust $1 > ${PWD##*/}".dust.fasta"
 #python3 /home1/boyd01z/MScProjectWork/RunScripts/FixFasta.py ${PWD##*/}".dust.fasta"
 
 printf "!!!Stats After Dust!!!\n"
